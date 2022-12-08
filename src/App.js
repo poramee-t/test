437fa9d608047dc001804188db76b18dcf10efa4
PP-ONTIME\Poramee_t <poramee.tar@gmail.com>
2022-12-08 09:54:14 +0700

    test2

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Hi Learn Odoo
+          Hello Learn Odoo
         </a>
       </header>
     </div>
